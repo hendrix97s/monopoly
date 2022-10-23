5b17f1d324ad77f664ffb15bb65c6561ee16e199
Luiz F. Lima <lf.system@outlook.com>
2022-10-23 20:50:10 -0300

    Created house model

---
+++ app/Http/Controllers/HouseController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHouseRequest;
+use App\Http\Requests\UpdateHouseRequest;
+use App\Models\House;
+
+class HouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreHouseRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreHouseRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function show(House $house)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(House $house)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateHouseRequest  $request
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateHouseRequest $request, House $house)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\House  $house
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(House $house)
+    {
+        //
+    }
+}
